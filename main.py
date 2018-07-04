acaa9893e751bf08fbd1c08be8f071e74e4885ca
arraiz <mikeldiezgarcia@gmail.com>
2018-07-04 17:02:24 +0200

    first commit

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+
+@app.route('/', subdomain="juegos")
+def juegos():
+    return 'Phaser3 jolasak'
+
+@app.route('/', subdomain="enigma")
+def enigma():
+    return 'Que sera'
+
+
 @app.route('/')
 def home():
     return 'Ongi etorri'
 
-@app.route("/", subdomain="juegos")
-def juegos():
-    return 'Phaser3 jolasak'
-
-@app.route("/", subdomain="enigma")
-def juegos():
-    return 'Que sera'
-
-
-
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
